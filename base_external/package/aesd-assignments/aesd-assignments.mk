6a8c9d3054c8e60209104b4b084ac4775bc9f5ef
jbohn3353 <jbohn3353@gmail.com>
2022-10-23 10:50:31 -0600

    add support for aesdchar package

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '826dcf1231043e03e805e59356516d7f40422b6f'
+AESD_ASSIGNMENTS_VERSION = '8c624144392ff8b428432113d9c833a8b84a8bca'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
---
+++ base_external/package/aesdchar/aesdchar.mk
@@ -0,0 +1,33 @@
+
+##############################################################
+#
+# AESDCHAR
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESDCHAR_VERSION = '8c624144392ff8b428432113d9c833a8b84a8bca'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESDCHAR_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-jbohn3353.git'
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
+AESDCHAR_MODULE_SUBDIRS += aesd-char-driver
+
+define AESDCHAR_CMDS
+	$(MAKE)
+endef
+
+# TODO copy .ko modules to target FS
+define AESDCHAR_INSTALL_TARGET_CMDS
+	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/aesdchar
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/etc/aesdchar/
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/etc/aesdchar/
+
+	$(INSTALL) -d 0755 $(TARGET_DIR)/lib/modules/5.15.18/extra/
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/*.ko $(TARGET_DIR)/lib/modules/5.15.18/extra/
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
